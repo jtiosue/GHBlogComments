c60379fb03d3d45107b258d21ca48ad7576c5327
jtiosue <jtiosue@gmail.com>
2021-02-18 14:04:26 -0500

    fix

--- ghcomments.js
+++ ghcomments.js
@@ -7,7 +7,7 @@ function parseMarkdown(markdownText) {
         .replace(/^### (.*$)/gim, '<h3>$1</h3>')
         .replace(/^## (.*$)/gim, '<h2>$1</h2>')
         .replace(/^# (.*$)/gim, '<h1>$1</h1>')
-        .replace(/^- (.*$)/gim, '<li style="padding-left: 2em;">$1</li>')
+        .replace(/^- (.*$)/gim, '<li style="margin-left: 2em;">$1</li>')
         .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
         .replace(/\*\*(.*)\*\*/gim, '<strong>$1</strong>')
         .replace(/\*(.*)\*/gim, '<em>$1</em>')
