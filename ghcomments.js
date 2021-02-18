9ec5ed7912ca21b30f2f1507f48d2209a6a3061a
jtiosue <jtiosue@gmail.com>
2021-02-17 23:50:42 -0500

    testing markdown to html

--- ghcomments.js
+++ ghcomments.js
@@ -1,5 +1,23 @@
 // adapted from https://aristath.github.io/blog/static-site-comments-using-github-issues-api
 
+// markdown to html
+// https://www.bigomega.dev/markdown-parser
+function parseMarkdown(markdownText) {
+    const htmlText = markdownText
+        .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+        .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+        .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+        .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+        .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+        .replace(/\*(.*)\*/gim, '<i>$1</i>')
+        .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+        .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+        .replace(/\n$/gim, '<br />')
+
+    return htmlText.trim()
+}
+
+
 // get attributes
 const repoName = document.currentScript.getAttribute("data-repoName");
 const issueNumber = document.currentScript.getAttribute("data-issueNumber");
@@ -66,7 +84,7 @@ function createCommentEl( response ) {
     user.innerHTML = response.user.login + ' (' + response.created_at.slice(0, response.created_at.search('T')) + ')';
     user.classList.add('user');
     commentContents.appendChild(user)
-    commentContents.innerHTML = commentContents.innerHTML + "<br/>" + response.body + "<br/>";
+    commentContents.innerHTML = commentContents.innerHTML + "<br/>" + parseMarkdown(response.body) + "<br/>";
 
     let comment = document.createElement('p');
     comment.setAttribute('data-created', response.created_at);
