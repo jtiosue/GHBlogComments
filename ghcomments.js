a08fd2003229dce8a54a20f3c62e2dbc6ff97cfb
Joseph T. Iosue <jtiosue@gmail.com>
2021-04-02 12:34:29 -0400

    Update ghcomments.js

--- ghcomments.js
+++ ghcomments.js
@@ -8,7 +8,7 @@ function parseMarkdown(markdownText) {
         .replace(/^## (.*$)/gim, '<h2>$1</h2>')
         .replace(/^# (.*$)/gim, '<h1>$1</h1>')
         .replace(/^- (.*$)/gim, '<li style="margin-left: 40px;">$1</li>')
-        .replace(/^\> > > (.*$)/gim, '<blockquote><blockquote><blockquote>$1</blockquote></blockquote>')
+        .replace(/^\> > > (.*$)/gim, '<blockquote><blockquote><blockquote>$1</blockquote></blockquote></blockquote>')
         .replace(/^\> > (.*$)/gim, '<blockquote><blockquote>$1</blockquote></blockquote>')
         .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
         .replace(/\*\*(.*)\*\*/gim, '<strong>$1</strong>')
